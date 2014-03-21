69dc0c478ea8b1a4766a1ae2fe49647c3f7f254c
Boye Borg <boye.borg@gmail.com>
2014-03-21 17:40:29 +0100

    added functionality

--- ipascript/ipascript.py
+++ ipascript/ipascript.py
@@ -1,11 +1,11 @@
 import commands
 import sys
-import smtplib
-import email.utils
-import string
-from email.mime.text import MIMEText
 
 def addUser(firstname,lastname,mail,username,mobile):
+	
+	if firstname == "" or lastname == "" or username == "" : return username + " not added. A required field is empty"
+	if mail == "" : mail = username
+	if mobile == "" : mobile = "00000000"
 
         c =     "ipa user-add " + username+ "  --first " + firstname + " --last " + \
                 lastname + " --shell /bin/sh --email " + mail + " --phone " + mobile + \
@@ -24,15 +24,25 @@ def addUser(firstname,lastname,mail,username,mobile):
         return "User added: " + username + " (Password sent to " + mail + ")"
 
 def addUserToGroup(group,user):
-        c = "ipa group-add-member " + group + " --users " + ",".join(users)
+        c = "ipa group-add-member " + group + " --users " + user
         s = commands.getoutput(c)
-
+        
         if "Failed members:" in s:
-                return s
-
-        return ",".join(users) + " added to " + group
+                if "no such entry" in s:
+                	return "User \"" + user + "\" not foud"
+                else:
+                	return "User \"" + user + "\" if already member of \"" + group + "\""
+	elif "ERROR" in s:
+		return "Group \"" + group + "\" not found"
+	
+        return user + " added to " + group
 
 def sendMail(mail,username,password,name):
+	import smtplib
+	import email.utils
+	import string
+	from email.mime.text import MIMEText
+	
         msgText = open('msgText.txt', 'r').read().split('{split}')
 
         msg = MIMEText(msgText[0]+username+msgText[1]+password+msgText[2])
@@ -48,13 +58,49 @@ def addOneUser():
 	firstname = raw_input("First Name: ")
 	lastname = raw_input("Last Name: ")
 	username = raw_input("Username: ")
-	mail = raw_input("Email: ")
+	mail = raw_input("Email ["+username+"@smint.no]: ")
 	mobile = raw_input("Mobile number: ")
 	
 	print addUser(firstname,lastname,mail,username,mobile)
 	
 def help():
-	print "not avaliable yet... see [ https://github.com/boyeborg/IPA-SMMDB ] for instructions"
+	return "not avaliable yet... see [ https://github.com/boyeborg/IPA-SMMDB/tree/master/ipascript ] for instructions"
+
+def deleteUser(username):
+	c = "ipa user-del " + username
+	s = commands.getoutput(c)
+	
+	if "ERROR" in s:
+		return "User \"" + username + "\" not found"
+	
+	return "User \"" + username + "\" deleted"
+	
+def addGroup(group,description):
+	c = "ipa group-add " + group + " --desc " + description
+	s = commands.getoutput(c)
+	
+	if "ERROR" in s:
+		if "invalid 'group_name'" in s:
+			return "\"" + group + "\" is not a valid group name"
+		else:
+			return "Group with name \"" + group + "\" already exists"
+	elif not "Added group" in s:
+		return "-----\nERROR!\n" + s + "\n-----"
+	
+	return "Group \"" + group + "\" added"
+	
+def deleteGroup(group):
+	c = "ipa group-del " + group
+	s = commands.getoutput(c)
+	
+	if "ERROR" in s:
+		return "Group \"" + group + "\" not found"
+	
+	return "Group \"" + group + "\" deleted"
+
+def addAllUsersToGroup(group):
+	#Add All users to group
+	return "All users added to group \"" + group + "\""
 	
 def addUsersFromFile(the_file):
 	f = open(thefile,"r")
@@ -76,27 +122,47 @@ def addUsersFromFile(the_file):
 		mobile = lineAry[4]
 		print addUser(firstname,lastname,mail,username,mobile)
 		if len(lineAry) == 6:
-			group = lineAry[5]
-			print addUserToGroup(group,[username])
+			groups = lineAry[5].spilt(",")
+			for group in groups:
+				print addUserToGroup(group,username)
 
 def argHandler(args):
-        if(len(args) == 0):
+        if len(args) == 0:
                 print "Please enter an argument"
         elif(arg[0] == "add-user"):
-        	#User add options
-                if(len(args) == 1):
+                if len(args) == 1:
                         addOneUser()
-                elif(len(args) == 2):
+                elif len(args) == 2:
                 	addUsersFromFile(args[1])
                 else:
-                     	print "Too many arguments. Expected one or two, "+len(args)+" given."
+                     	print "Too many arguments. Expected one or two, " + len(args) + " given."
         elif(args[0] == "del-user"):
-		##nothing yet
-		return
+		if len(args) == 2:
+			print deleteUser(args[1])
+		else:
+			print "Expected two arguments, " + len(args) + " given"
+	elif(args[0] == "add-group"):
+		if len(args) == 3:
+			print addGroup(args[1],args[2])
+		else:
+			print "Expected three arguments, " + len(args) + " given"
+	elif(args[0] == "del-group"):
+		if len(args) == 2:
+			print deleteGroup(args[1])
+		else:
+			print "Expected two arguments, " + len(args) + " given"
+	elif(args[0] == "add-all"):
+		if len(args) == 2:
+			warning = "This will add all users to the group \"" + args[1] + "\". Are you sure you want to continue [Y/N]? "
+			answer = input(warning)
+			while answer.upper() != "Y":
+				if answer.upper() == "N":
+					return
+				answer = input(warning)
+			print addAllUsersToGroup(group)
         elif(args[0] == "help"):
-        	help()
-                
+        	print help()
         else:
-             	print "Not valid argument, try <ipascript help> for help"
+             	print "Not valid argument, try \"help\" for help"
 
 argHandler(sys.argv[1:])
