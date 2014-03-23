dfcd69339348437ebaebac8711082d7265ef6c92
Aydex <adrian.hundseth@hotmail.com>
2014-03-23 14:08:40 +0100

    now with last names

--- src/main/java/com/springapp/mvc/JSONController.java
+++ src/main/java/com/springapp/mvc/JSONController.java
@@ -1,37 +1,74 @@
 package com.springapp.mvc;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 /**
  * Created by adrianh on 12.02.14.
  */
 
 @Controller
-@RequestMapping("/json")
+@RequestMapping("/")
 public class JSONController {
     private PersonList personList = new PersonList();
 
-    @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody
-    Person getPerson() {
-        Person person = personList.getPersonById(1);
-//        person.setName("Test");
-//        person.setEmail("test@test.test");
-//        person.setId(1);
-        return person;
+    @RequestMapping
+    public String defaultReturn() {
+        return "index";
     }
 
-    @RequestMapping(value = "person", method = RequestMethod.POST)
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     public @ResponseBody Person post(@RequestBody final Person person) {
-        System.out.print(person.getId() + " " + person.getName());
+        System.out.print(person.getId() + " " + person.getFirstName());
+        personList.addPerson(person);
         return person;
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public @ResponseBody Person getPersonById(@PathVariable("id") int id) {
+    @RequestMapping(value = "/addList", method = RequestMethod.POST)
+    public @ResponseBody ArrayList<Person> listPost(@RequestBody final ArrayList<Person> list) {
+        for (Person person : list) {
+            personList.addPerson(person);
+        }
+        return personList.getPersonList();
+    }
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public @ResponseBody ArrayList<Person> getList() {
+        return personList.getPersonList();
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public @ResponseBody Person getPersonById(@PathVariable int id) {
         return personList.getPersonById(id);
     }
+
+    @RequestMapping(value = "/memberOf/{membership}", method = RequestMethod.GET)
+    public @ResponseBody ArrayList<Person> personsByMembership(@PathVariable String membership) {
+        ArrayList<Person> retList = new ArrayList<Person>();
+        for (Person person : personList.getPersonList()) {
+            if (person.getMembership().compareTo(membership) == 0) {
+                retList.add(person);
+            }
+        }
+        return retList;
+    }
+
+    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
+    public @ResponseBody String listUsersJson() throws JSONException {
+        JSONArray userArray = new JSONArray();
+        for (Person user : personList.getPersonList()) {
+            JSONObject userJSON = new JSONObject();
+            userJSON.put("id", user.getId());
+            userJSON.put("name", user.getFirstName());
+            userJSON.put("lastname", user.getLastName());
+            userJSON.put("email", user.getEmail());
+            userArray.put(userJSON);
+        }
+        return userArray.toString();
+    }
 }
-
-
