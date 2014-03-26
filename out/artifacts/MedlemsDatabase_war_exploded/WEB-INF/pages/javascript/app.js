677e4222e98a9c72bbcd1629656293a286abeb59
Aydex <adrian.hundseth@hotmail.com>
2014-03-26 18:32:59 +0100

    new .war

---
+++ out/artifacts/MedlemsDatabase_war_exploded/WEB-INF/pages/javascript/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Kristian on 12/02/14.
+ */
+var app = angular.module('mdbApp',["ngResource", "ngRoute"]);
+
+    app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider
+                .when('/user', {
+                    templateUrl: 'views/user.html',
+                    controller: 'UserCtrl'
+                })
+                .when('/', {
+                    templateUrl: 'views/person.html',
+                    controller: 'PersonCtrl'
+                })
+
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }
+    ]);
