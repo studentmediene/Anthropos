dd8965d01589e6b5011a9cf82351ed60d5f9b7fc
root <root@ipamail.studentmediene.local>
2014-03-26 22:13:42 +0100

    Bugs have been exterminated

--- ipascript/ipascript.py
+++ ipascript/ipascript.py
@@ -1,5 +1,10 @@
 #-*- coding: utf-8 -*-
 import commands
+import smtplib
+import email.utils
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
 
 
 def user_exists(term, value):
@@ -80,16 +85,16 @@ def add_group(group, description):
 
 
 def send_password(mail, username, password, name):
-    import smtplib
-    import email.utils
-    from email.mime.text import MIMEText
 
     msg_text = open('msgText.txt', 'r').read().split('{split}')
 
-    msg = MIMEText(msg_text[0] + username + msg_text[1] + password + msg_text[2])
+    Body = MIMEText(msg_text[0] + username + msg_text[1] + password + msg_text[2])
+    msg = MIMEMultipart()
     msg['To'] = email.utils.formataddr((name, mail))
     msg['From'] = email.utils.formataddr(('IT-Drift', 'it-drift@studentmediene.no'))
-    msg['Subject'] = "Brukerkonto"
+    msg['Subject'] = 'Brukerkonto'
+    msg['Reply-to'] = 'bruker@studentmediene.no'
+    msg.attach(Body)
 
     server = smtplib.SMTP('mail.studentmediene.local')
     server.sendmail('it-drift@studentmediene.no', [mail], msg.as_string())
